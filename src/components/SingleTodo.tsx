1eadcde91a254495eb8412261b408214517448a9
Kapil Jituri <kapiljituri@gmail.com>
2022-06-02 15:37:51 -0700

    Handle delete and done in todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -11,15 +11,37 @@ type Props = {
 }
 
 const SingleTodo = ({todo, todos, setToDos}: Props) => {
-  return (
-    <form className='todos_single'>
-      <span className='todos_single_text'>{todo.todo}</span>
-      
-      <div className='icon'><GrEdit/></div>
-      <div className='icon'><MdDelete/></div>
-      <div className='icon'><MdDoneOutline/></div>
-    </form>
-  )
+
+    const handleDone = (id: number) => {
+        setToDos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, isDone: !todo.isDone }: todo
+            )
+        );
+    };
+
+    const handleDelete = (id: number) => {
+        setToDos(todos.filter((todo) => todo.id !== id));
+    };
+    return (
+        <form className='todos_single'>
+            {
+                todo.isDone ? (
+                    <s className='todos_single_text'>{todo.todo}</s>
+                ) : (
+                    <span className='todos_single_text'>{todo.todo}</span>
+                )
+            }
+            
+            <div className='icon'><GrEdit/></div>
+            <div className='icon' onClick={()=>handleDelete(todo.id)}>
+                <MdDelete/>
+            </div>
+            <div className='icon' onClick={()=>handleDone(todo.id)}>
+                <MdDoneOutline/>
+            </div>
+        </form>
+    )
 }
 
 export default SingleTodo
