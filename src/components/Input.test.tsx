8f0c64c6f6b255c3882952b3119ac82ef8c362bb
Kapil Jituri <kapiljituri@gmail.com>
2022-06-09 13:55:31 -0700

    Added test to see if componets rendered correctly

--- src/components/Input.test.tsx
+++ src/components/Input.test.tsx
@@ -3,6 +3,17 @@ import Input from './Input'
 
 describe("Input Component", () => {
     it('rendered input', () => {
-        const {} = render(<Input showDiv={false}/>);
+        const { getByTestId } = render(<Input showDiv={false}/>);
+        expect(getByTestId('searchBar')).toBeTruthy();
+    });
+
+    it('rendered div', () => {
+        const { getByTestId } = render(<Input showDiv={true}/>);
+        expect(getByTestId('divWeWantToShow')).toBeTruthy();
+    });
+
+    it('not rendered div', () => {
+        const { queryByTestId } = render(<Input showDiv={false}/>);
+        expect(queryByTestId('divWeWantToShow')).toBeFalsy();
     });
 });
